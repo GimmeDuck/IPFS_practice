19953b8b656f5c31461639028477292538b227a8
lydiacho <sheecho0804@ewhain.net>
2022-05-14 02:33:42 +0900

    API key 인증 완료

---
+++ main.js
@@ -0,0 +1,10 @@
+const pinataSDK = require('@pinata/sdk');
+const pinata = pinataSDK('71084a474f5f077b9699', '5d9f0655f5b4194f2e0bfac0eff6fe0005a242c8af72bed24cf78cb61619d7da');
+
+pinata.testAuthentication().then((result) => {
+    //handle successful authentication here
+    console.log(result);
+}).catch((err) => {
+    //handle error here
+    console.log(err);
+});
